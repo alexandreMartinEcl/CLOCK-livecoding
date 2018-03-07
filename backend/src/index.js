4e2c90e316bea6f6dd20150bbc0d4d77a22e84d3
Rémy Prioul <remy.prioul@gmail.com>
2018-03-07 14:27:55 +0100

    added cors dependencies required for requests to api (#14)

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,13 @@ const express = require('express');
 const serveStatic = require('serve-static');
 const config = require('./config');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const server = require('http').Server(app);
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/api', require('./api'));
 
