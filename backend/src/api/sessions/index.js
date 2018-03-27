33b33ef416a5899b3b8d8a709497316682851271
Louis Hulot <louishulot@hotmail.fr>
2018-03-27 14:14:31 +0200

    add apiDoc api/sessions (#37)

--- backend/src/api/sessions/index.js
+++ backend/src/api/sessions/index.js
@@ -7,6 +7,27 @@ const sessionController = require('./sessionController');
 const codeController = require('./codeController.js');
 
 // get requests
+/**
+ * @apiVersion 1.0.0-SNAPSHOT
+ * @api {get} sessions get-all-sessions
+ * @apiDescription récupère l'ensemble des sessions disponibles
+ * @apiName get-all-sessions
+ * @apiGroup Session
+ * @apiHeader {String} Authorization JWT token
+ * @apiHeaderExample {json} Header-Example:
+ * {
+ * "Authorization":"JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTZmMDlkYzM1YmZkZTBm"
+ * }
+ * @apiSuccess {Boolean} success succès
+ * @apiSuccess {array[Session]} sessions array contenant les sessions
+ * @apiSuccessExample {json} Success-Response:
+ *{
+    "success": true,
+    "sessions": [session1, session2, ...]
+  }
+ *
+ * @apiError (401) There are no sessions
+ */
 router.get('/', sessionController.findAll);
 router.get('/:hash', sessionController.findOne);
 
