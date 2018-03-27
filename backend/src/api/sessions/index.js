affb36afc3a315efb6fe57af9cce89e13a4fb615
Louis Hulot <louishulot@hotmail.fr>
2018-03-27 16:00:33 +0200

    apiDoc multiple input (#40)
    * apiDoc multiple input
    
    * fixing eslint

--- backend/src/api/sessions/index.js
+++ backend/src/api/sessions/index.js
@@ -23,16 +23,154 @@ const codeController = require('./codeController.js');
  * @apiSuccessExample {json} Success-Response:
  *{
     "success": true,
-    "sessions": [session1, session2, ...]
+    "sessions": [sessionSchema, sessionSchema, ...]
   }
  *
- * @apiError (401) There are no sessions
+ * @apiError (401) session there are no sessions
  */
 router.get('/', sessionController.findAll);
+
+/**
+ * @apiVersion 1.0.0-SNAPSHOT
+ * @api {get} sessions/:hash get-one-session
+ * @apiDescription récupère la session au hash code envoyé
+ * @apiName get-one-session
+ * @apiGroup Session
+ * @apiHeader {String} Authorization JWT token
+ * @apiHeaderExample {json} Header-Example:
+ * {
+ * "Authorization":"JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTZmMDlkYzM1YmZkZTBm"
+ * }
+ * @apiParam {String} hash hash code de la session
+ * @apiSuccess {Boolean} success succès
+ * @apiSuccess {sessionSchema} session la session demandé
+ * @apiSuccessExample {json} Success-Response:
+ *{
+    "success": true,
+    "session": {
+        "created": "2018-03-22T08:18:23.095Z",
+        "users": [
+            {
+                "html": "",
+                "css": "",
+                "js": "",
+                "user": {
+                    "_id": "5ab367455813230018e9e8c0",
+                    "username": "amartin",
+                    "role": "etudiant",
+                    "nom": "nom",
+                    "prenom": "prenom",
+                    "email": "nom.prenom.@gmail.com"
+                },
+                "_id": "5ab367455813230018e9e8c1"
+            }
+        ],
+        "_id": "5ab367455813230018e9e8c2",
+        "creator": {
+            "_id": "5ab367455813230018e9e8bf",
+            "username": "nom.prenom",
+            "role": "etudiant",
+            "nom": "nom",
+            "prenom": "prenom",
+            "email": "nom.prenom@gmail.com"
+        },
+        "name": "CLOCK sa mère",
+        "hash": "zqEtPX",
+        "__v": 0
+    }
+}
+ *
+ * @apiError (401) session session does not exist
+ */
 router.get('/:hash', sessionController.findOne);
 
 router.get('/users/:hash', codeController.findAllUsersOfSession);
+
+/**
+ * @apiVersion 1.0.0-SNAPSHOT
+ * @api {get} sessions/code/:hash get-all-users-of-session
+ * @apiDescription récupère l'ensemble des users d'une sessuib
+ * @apiName get-all-users-of-session
+ * @apiGroup User
+ * @apiHeader {String} Authorization JWT token
+ * @apiHeaderExample {json} Header-Example:
+ * {
+ * "Authorization":"JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTZmMDlkYzM1YmZkZTBm"
+ * }
+ * @apiParam {String} hash hash code de la session
+ * @apiSuccess {Boolean} success succès
+ * @apiSuccess {result} result object contenant hash, creator, created, name, users, code
+ * @apiSuccess {String} result.hash hash code de la session
+ * @apiSuccess {userSchema} result.creator objet user du créateur de la session
+ * @apiSuccess {date} cresult.reated date de création de la session
+ * @apiSuccess {String} result.name nom de la session
+ * @apiSuccess {array[usersSchema]} result.usesrs array des userSchema de la session
+ * @apiSuccess {code} result.code tableau contenant le cade
+ * @apiSuccessExample {json} Success-Response:
+ *{
+    "success": true,
+    "result": {
+        "hash": "zqEkPX",
+        "creator": userSchema,
+        "created": "2018-03-22T08:18:23.095Z",
+        "name": "CLOCK sa mère",
+        "users": [userSchema, userSchema, ...],
+        "code": {
+            "hmtl": "<html>\n  <p>\n      Eloïse arrêtes tes bêtises\n  </p>\n</html>\n\n",
+            "css": "p {\n  color: green;\n}",
+            "js": ""
+        }
+    }
+}
+ *
+ * @apiError (401) session session does not exist
+ */
 router.get('/code/:hash', codeController.findUserSessionInfo);
+
+/**
+ * @apiVersion 1.0.0-SNAPSHOT
+ * @api {get} sessions/code/:hash/: get-one-user-of-session
+ * @apiDescription récupère les informations d'un utilisateur d'une session
+ * @apiName get-all-users-of-session
+ * @apiGroup User
+ * @apiHeader {String} Authorization JWT token
+ * @apiHeaderExample {json} Header-Example:
+ * {
+ * "Authorization":"JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YTZmMDlkYzM1YmZkZTBm"
+ * }
+ * @apiParam {String} hash hash code de la session
+ * @apiParam {String} username user name de l'utilisateur dont on veut récuperer les informations
+ * @apiSuccess {Boolean} success succès
+ * @apiSuccess {object} result objet contenant, result et user
+ * @apiSuccess {object} result.user object contenant html, css, js, user
+ * @apiSuccess {String} result.user.html code html de l'utilisateur
+ * @apiSuccess {String} result.user.js code js de l'utilisateur
+ * @apiSuccess {String} result.user.css code css de l'utilisateur
+ * @apiSuccess {userSchema} result.user.user userSchema de l'utilisateur
+ * @apiSuccessExample {json} Success-Response:
+ *{
+    "success": true,
+    "result": {
+        "success": true,
+        "user": {
+            "html": "",
+            "css": "p {\n  color: red;\n}",
+            "js": "",
+            "user": {
+                "_id": "5ab367455813230018e9e8c0",
+                "username": "amartin",
+                "role": "etudiant",
+                "nom": "Martin",
+                "prenom": "Alexandre",
+                "email": "alexandre.martin.ecl@gmail.com"
+            },
+            "_id": "5ab367455813230018e9e8c1"
+        }
+    }
+}
+ *
+ * @apiError (401) session session does not exist
+ */
 router.get('/code/:hash/:username', codeController.findUserCode);
 
 // post requests
